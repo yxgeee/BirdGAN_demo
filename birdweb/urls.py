1c428f87415ebe914f2bb297c12626dfd481a506
gyxoned <geyixiao831@gmail.com>
2018-04-13 15:30:39 +0800

    update

--- birdweb/urls.py
+++ birdweb/urls.py
@@ -19,10 +19,5 @@ from . import view,search
 urlpatterns = [
     url(r'^$', view.index),
     url(r'^describe_text/$', view.describe_text, name = 'describe_text'),
-    # url(r'^$', search.search_form),
-    # url(r'^search$', search.search),
-
-    # url(r'^search-post$', search.search_post),
-    # url(r'^ajax_list/$', view.ajax_list, name='ajax-list'),
-    # url(r'^ajax_dict/$', view.ajax_dict, name='ajax-dict'),
+    url(r'^random_text/$', view.random_text, name = 'random_text'),
 ]
--- birdweb/view.py
+++ birdweb/view.py
@@ -4,20 +4,16 @@ from django.template import RequestContext
 from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+
 import json
+import random
 
-
-# from blog.models import Post
-
-
-# def hello(request):
-# 	return HttpResponse("Helldcdo world ! ")
+texts = ["this small bird has a yellow crown and a white belly.",
+		"this bird has a speckled belly and breast with a short pointy bill."]
 
 def index(request):
 	context = {}
 	return render(request, 'base.html', context)
-	# posts = Post.objects.all()
-    # return render_to_response("blog/index.html", {"posts": posts}, context_instance=RequestContext(request))
 
 @csrf_exempt
 def describe_text(request):
@@ -27,10 +23,6 @@ def describe_text(request):
 					'img4':'/static/results/4.png', 'img5':'/static/results/5.png'}
 	return HttpResponse(json.dumps(return_json), content_type='application/json')
 
-# def ajax_list(request):
-#     a = range(100)
-#     return JsonResponse(a, safe=False)
- 
-# def ajax_dict(request):
-#     name_dict = {'twz': 'Love python and Django', 'zqxt': 'I am teaching Django'}
-#     return JsonResponse(name_dict)
+def random_text(request):
+	return_json = {'text':random.choice(texts)}
+	return HttpResponse(json.dumps(return_json), content_type='application/json')
