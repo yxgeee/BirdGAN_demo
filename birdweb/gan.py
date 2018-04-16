f63ddae0a112b5b60f55b1070b4351a8447b2190
gyxoned <geyixiao831@gmail.com>
2018-04-16 13:25:10 +0800

    version 1.0

---
+++ birdweb/gan.py
@@ -0,0 +1,25 @@
+import torch
+import os,sys
+
+dir_path = (os.path.abspath(os.path.join(os.path.realpath(__file__), '../../AttnGAN/code')))
+sys.path.append(dir_path)
+from generate import generate_imgs,gen_example
+
+__all__ = ["gan"]
+
+_g = None
+
+class _Attngan(object):
+	def __init__(self, cfg_file):
+		wordtoix, algo=generate_imgs(cfg_file)
+		self.wordtoix = wordtoix
+		self.algo = algo
+		
+	def gan(self, sent):
+		return gen_example(self.wordtoix, self.algo, sent)
+
+if _g is None:
+    _g = _Attngan('AttnGAN/code/cfg/eval_bird.yml')
+
+def gan(sent):
+    return _g.gan(sent)
--- birdweb/view.py
+++ birdweb/view.py
@@ -5,11 +5,20 @@ from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from gan import gan
+
 import json
 import random
+import os,sys
+import pdb
 
-texts = ["this small bird has a yellow crown and a white belly.",
-		"this bird has a speckled belly and breast with a short pointy bill."]
+dir_path = (os.path.abspath(os.path.join(os.path.realpath(__file__), '../../AttnGAN/code')))
+sys.path.append(dir_path)
+from generate import generate_imgs
+
+texts = ["the bird is small with a pointed bill, has black eyes, and a yellow crown.",
+		"this particular bird has a gray belly breast and sides and a greenish yellow strip on its back",
+		"this bird has wings that are grey and has a yellow body"]
 
 def index(request):
 	context = {}
@@ -17,12 +26,25 @@ def index(request):
 
 @csrf_exempt
 def describe_text(request):
-	text = request.POST.get('description')
-	return_json = {'img0':'/static/results/0.png', 'img1':'/static/results/1.png',
-					'img2':'/static/results/2.png', 'img3':'/static/results/3.png',
-					'img4':'/static/results/4.png', 'img5':'/static/results/5.png'}
+	text = request.POST['description']
+	# return_code = generate_imgs(text,'AttnGAN/code/cfg/eval_bird.yml')
+	return_code = gan(text)
+	if return_code==0:
+		response = JsonResponse({"error": "输入不能为空"})
+		response.status_code = 403 # To announce that the user isn't allowed to publish
+		return response
+
+	# TODO random order
+	return_json = {'img0':'/static/results/gan_0.png', 'img1':'/static/results/gan_1.png',
+					'img2':'/static/results/gan_2.png', 'img3':'/static/results/gan_3.png',
+					'img4':'/static/results/gan_4.png', 'img5':'/static/results/gan_5.png'}
 	return HttpResponse(json.dumps(return_json), content_type='application/json')
 
+@csrf_exempt
 def random_text(request):
-	return_json = {'text':random.choice(texts)}
+	text = request.POST['description']
+	texts1 = list(texts)
+	if text in texts1:
+		texts1.remove(text)
+	return_json = {'text':random.choice(texts1)}
 	return HttpResponse(json.dumps(return_json), content_type='application/json')
